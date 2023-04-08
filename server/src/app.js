c4384d43c5e4ad6142736d727c62361ad87f99b9
Daniel Aguila <danielbaguila@gmail.com>
2023-04-08 18:45:19 +0900

    Set up database connection

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,19 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const morgan = require("morgan")
+const { Client } = require('pg')
+const client = new Client()
+/*
+await client.connect()
+ 
+const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+console.log(res.rows[0].message) // Hello world!
+await client.end()
+*/
+
+/*
+
+*/
 
 const app = express() //builds an express server
 app.use(morgan("combined"))
